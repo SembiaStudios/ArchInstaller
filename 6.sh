52bdfcb7a9722a3826a31c14e416860b43e0df71
SembiaStudios <91810880+SembiaStudios@users.noreply.github.com>
2023-07-01 10:03:24 +0200

    Create 6.sh

---
+++ 6.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------------------------
+#     
+#    Sembia Studios  
+#    
+#  Arch Linux Post Install Setup and Config
+#-------------------------------------------------------------------------
+
+echo
+echo "Installiere SOFTWARE"
+echo
+
+PKGS=(
+  # System
+
+  'linux-lts'
+
+  # Terminal
+
+  'bash-completion'
+  'curl'
+  'git'
+  'htop'
+  'neofetch'
+  'unzip'
+  'unrar'
+  'xfce4-terminal'
+
+  #Disk
+
+  'gparted'
+
+  # General
+
+  'nautilus'
+  'variety'
+  'conky'
+
+  # Dev
+
+  'gcc'
+  'nodejs'
+  'nodejs'
+  'npm'
+  'yarn'
+  'code'
+
+  # Web
+  'firefox'
+
+  # Chat
+  'discord'
+
+  # Media
+  'vlc'
+  'simplescreenrecorder'
+  'xfce4-screenshooter'
+  'kdenlive'
+
+  # Graphic & Design
+
+  'krita'
+  'ristretto'
+
+  # Prod
+
+  'galculator'
+  'mousepad'
+  'xpdf'
+  'libreoffice-fresh'
+
+  # VM
+   'virtualbox'
+   'virtualbox-host-modules-arch'
+)
+
+for PKG in "${PKGS[@]}"; do
+    echo "Installiert: ${PKG}"
+    sudo pacman -S "$PKG" --noconfirm --needed
+done
+
+echo
+echo "Fertig!"
+echo
