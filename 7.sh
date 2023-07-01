50658edd88dc88983407760ffc5059ecd93a6f7f
SembiaStudios <91810880+SembiaStudios@users.noreply.github.com>
2023-07-01 10:08:32 +0200

    Create 7.sh

---
+++ 7.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------------------------
+#     
+#    Sembia Studios  
+#    
+#  Arch Linux Post Install Setup and Config
+#-------------------------------------------------------------------------
+
+echo
+echo "Installiere AUR SOFTWARE"
+echo
+
+cd "${HOME}"
+
+echo "Klaue: AURIC von anderen xD"
+git clone "https://github.com/rickellis/AURIC.git"
+
+PKGS=(
+  # System
+  
+  'menulibre'
+
+  # UTILS
+
+  'enpass-bin'
+  'oomox'
+
+  # Media
+
+  'spotify'
+
+  # Themes
+
+    'gtk-theme-arc-git'
+    'adapta-gtk-theme-git'
+    'paper-icon-theme'
+    'tango-icon-theme'
+    'tango-icon-theme-extras'
+    'numix-icon-theme-git'
+    'sardi-icons'
+)
+
+cd ${HOME}/AURIC
+chmod +x auric.sh
+
+for PKG in "${PKGS[@]}"; do
+    ./auric.sh -i $PKG
+done
+
+echo
+echo "Fertig!"
+echo
